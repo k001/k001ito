1fb0e988535ee2f6c713cd32ff5735f1c2b272a5
Ivan Zenteno <k001@@users.noreply.github.com>
2016-06-16 22:30:46 -0500

    last code

--- plugins/exchange/exchange.py
+++ plugins/exchange/exchange.py
@@ -10,11 +10,11 @@ def process_message(data):
     global exchange
     channel = data["channel"]
     text = data["text"]
-    if channel.startswith("C04JUEPP0"):
-         if text.startswith("exchange"):
-              outputs.append([channel, "Searching...:thinking_face:"])
-              exchange[channel].append(text[9:])
-              url = "http://api.fixer.io/latest?symbols=MXN&base=USD"
-              resp = requests.get(url=url)
-              data = json.loads(resp.text)
-    outputs.append(["D0JJGQRJ9", data])
+    if text.startswith("exchange"):
+          exchange[channel] = []
+          outputs.append([channel, "Searching...:thinking_face:"])
+          exchange[channel].append(text[9:])
+          url = "http://api.fixer.io/latest?symbols=MXN&base=USD"
+          resp = requests.get(url=url)
+          data = json.loads(resp.text)
+          outputs.append([channel, data])
